9ad87eb784e520301928971e020e227309438ac8
tugsad-aydin <tugsad.aydin@icredible.com>
2025-06-21 00:22:50 +0300

    rebased

--- src/wv_mem.h
+++ src/wv_mem.h
@@ -1,7 +1,14 @@
 #ifndef _WV_MEM_H
 #define _WV_MEM_H
 
+#if defined(WIN32) || defined(_WIN32)
+#include <windows.h>
 
 
+#elif defined(__linux__) || defined(__linux)
+
+#elif defined(__APPLE__) || defined(__MACH__)
+
+#endif // OS checks
 
 #endif // _WV_MEM_H
